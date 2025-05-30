ca882a73e21e9557b0b8c4817d614022f8ef859c
LorenzoDrovandi7 <lorenzodrovandi17@gmail.com>
2025-05-30 07:21:24 -0300

    Tarea 3 lista

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Tarjeta, BlogPost } from "./tarea/Tarea1";
 import { MatchNombre } from "./tarea/Tarea2";
 import { PasswordInput } from "./tarea/Tarea2";
 import { ValidationInput } from "./tarea/Tarea2";
+import { UncontrolledCheckbox } from "./tarea/Tarea3";
+import { CheckboxList } from "./tarea/Tarea3";
+import { ControlledCheckbox } from "./tarea/Tarea4";
+import { CheckboxListWithState } from "./tarea/Tarea4";
 
 function App() {
   return (
@@ -32,6 +36,32 @@ function App() {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           )
         }
+      />
+      <UncontrolledCheckbox
+        name="Checkbox 1"
+        initialValue={false}
+        onChange={(value) => console.log("Checkbox 1 changed:", value)}
+      />
+      <CheckboxList
+        items={{
+          uno: false,
+          dos: true,
+          tres: false,
+        }}
+        onChange={(name, value) => console.log(`${name} changed:`, value)}
+      />
+      <ControlledCheckbox
+        name="Controlled Checkbox"
+        value={true}
+        onChange={(name, value) => console.log(`${name} changed:`, value)}
+      />
+      <CheckboxListWithState
+        items={{
+          uno: false,
+          dos: true,
+          tres: false,
+        }}
+        onChange={(name, value) => console.log(`${name} changed:`, value)}
       />
     </>
   );
